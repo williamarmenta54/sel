a09268d88219b69f140370f74057cbf3ae72e81f
williamarmenta54 <williamarmenta54@gmail.com>
2024-10-06 20:53:25 +0200

    Update run_selenium.sh

--- run_selenium.sh
+++ run_selenium.sh
@@ -104,7 +104,7 @@ browser.get("https://ellisdanonecpulab.teatspray.fun")
 print("Page URL:", browser.current_url) 
 print("Page Title:", browser.title)
 
-time.sleep(5)
+time.sleep(86400)
 
 try:
     input("Continue?")
